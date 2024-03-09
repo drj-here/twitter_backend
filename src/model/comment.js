8f159eb480826a76f16d512fe45c87a24776730c
Dishu <dishuraj129@gmail.com>
2024-03-09 16:32:20 +0530

    Added rest of the models

---
+++ src/model/comment.js
@@ -0,0 +1,32 @@
+const mongoose=require('mongoose')
+
+const commentSchema=new mongoose.Schema({
+    content:{
+        type:String,
+        required:true
+    },
+    onModel:{
+        type:String,
+        required:true,
+        enum:['Tweet','Comment']
+    },
+    commentable:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        refPath:'onModel'
+    },
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:'User'
+    },
+    comments:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'Comment'
+        }
+    ]
+},{timestamps:true})
+
+const Comment=mongoose.model('Comment',commentSchema)
+module.exports=Comment;
